1468321827d0a54edb389c9206759c58021db076
AnshKapoor007 <kapoor.ansh28@gmail.com>
2023-07-27 15:20:52 +0530

    UI Updated

--- frontend/script.js
+++ frontend/script.js
@@ -17,15 +17,14 @@ document.getElementById('urlForm').addEventListener('submit', async (event) => {
 
     const data = await response.json();
     displayResult(data.result);
-    console.log(data.result);
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+  } catch (error) { }
 });
 
 function displayResult(result) {
   const resultElement = document.createElement('p');
-  document.getElementById('result-container').style.display = "block";
+  document.getElementById('result-container').style.display = 'block';
+  document.getElementById('result-container').classList.remove(result[0] === 'bad' ? 'bg-green' : 'bg-red');
+  document.getElementById('result-container').classList.add(result[0] === 'bad' ? 'bg-red' : 'bg-green');
   resultElement.textContent = `Website is ${result}.`;
   resultElement.style.marginTop = '20px';
   resultElement.style.fontWeight = 'bold';
